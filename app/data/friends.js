dfcdf528658099e5c4d5d17d15860894548498dd
Fooji <jfujitsubo@yahoo.com>
2019-03-20 16:28:46 -0700

    bg fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArr = [
     {
         name: 'Fuji',
-        photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F614810%2Fpexels-photo-614810.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fface%2F&docid=Bg0wwhYqbH9NmM&tbnid=HVo2yp17IUCWqM%3A&vet=10ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhpKAAwAA..i&w=500&h=427&bih=695&biw=672&q=face%20photo&ved=0ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhpKAAwAA&iact=mrc&uact=8',
+        photo: 'https://pbs.twimg.com/profile_images/1053055123193122816/IUwo6l_Q_400x400.jpg',
         scores: [
             '1',
             '2',
@@ -17,7 +17,7 @@ var friendsArr = [
     },
     {
         name: 'Coleman',
-        photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.uni-regensburg.de%2FFakultaeten%2Fphil_Fak_II%2FPsychologie%2FPsy_II%2Fbeautycheck%2Fenglish%2Fdurchschnittsgesichter%2Fm(01-32)_gr.jpg&imgrefurl=https%3A%2F%2Fwww.uni-regensburg.de%2FFakultaeten%2Fphil_Fak_II%2FPsychologie%2FPsy_II%2Fbeautycheck%2Fenglish%2Fdurchschnittsgesichter%2Fdurchschnittsgesichter.htm&docid=pLaAIGCYU-AW7M&tbnid=aY4YUE1-ZhtbuM%3A&vet=10ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhsKAMwAw..i&w=320&h=400&bih=695&biw=672&q=face%20photo&ved=0ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhsKAMwAw&iact=mrc&uact=8',
+        photo: 'https://pbs.twimg.com/profile_images/1053055123193122816/IUwo6l_Q_400x400.jpg',
         scores: [
             '1',
             '5',
@@ -33,7 +33,7 @@ var friendsArr = [
     },
     {
         name: 'Kelsey',
-        photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F415829%2Fpexels-photo-415829.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fface%2F&docid=Bg0wwhYqbH9NmM&tbnid=n1o_2zbA09JEkM%3A&vet=10ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhrKAIwAg..i&w=500&h=441&bih=695&biw=672&q=face%20photo&ved=0ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhrKAIwAg&iact=mrc&uact=8',
+        photo: 'https://pbs.twimg.com/profile_images/1053055123193122816/IUwo6l_Q_400x400.jpg',
         scores: [
             '1',
             '2',
@@ -49,11 +49,10 @@ var friendsArr = [
     },
     {
         name: 'Haley',
-        photo: 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.makeup.com%2F-%2Fmedia%2Fimages%2Fmakeup%2F2018%2Fseptember%2F11-ultimate-makeup%2Fthe-ultimate-makeup-tutorial-eyeshadow-lipstick-face-hero-mudc-091118_r2.jpg&imgrefurl=https%3A%2F%2Fwww.makeup.com%2Fthe-ultimate-makeup-tutorial&docid=edQU0Gn6iJSNaM&tbnid=oAkseATDCFqB6M%3A&vet=10ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhtKAQwBA..i&w=920&h=690&bih=695&biw=672&q=face%20photo&ved=0ahUKEwinrJ68x5HhAhVkMH0KHSs7AncQMwhtKAQwBA&iact=mrc&uact=8',
+        photo: 'https://pbs.twimg.com/profile_images/1053055123193122816/IUwo6l_Q_400x400.jpg',
         scores: [
             '3',
-            '2',
-            '3',
+            '2',,
             '4',
             '3',
             '3',
